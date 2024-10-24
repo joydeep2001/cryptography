a455ecb35298ff049b6506dff9bd502d1dd3cf5f
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-10-24 21:24:56 +0530

    utils: Add Unit test.

---
+++ test/test_utils.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <cstdint>
+#include <vector>
+#include "../include/Utils.hpp"
+
+using namespace std;
+
+
+TEST(CharPolyMask, CreateCharPolyMask) 
+{
+    //x^8 + x^4 + x^3 + x^2 + 1
+    vector<int> terms = {8, 4, 3, 2, 1};
+    uint8_t mask = createPoly<uint8_t>(terms);
+    EXPECT_EQ(mask, 0b00011110);
+} 
+
+TEST(Popcount, PopcountTest)
+{
+    EXPECT_EQ(popcount<uint8_t>(8), 1);
+    EXPECT_EQ(popcount<uint8_t>(9), 2);
+    EXPECT_EQ(popcount<uint8_t>(255), 8);
+}
