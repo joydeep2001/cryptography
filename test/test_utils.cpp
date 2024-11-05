698aae190252b741b6ad639b553bb420a2d4d988
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-11-05 19:39:59 +0530

    fix CharPolyMask test.

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -9,9 +9,9 @@ using namespace std;
 TEST(CharPolyMask, CreateCharPolyMask) 
 {
     //x^8 + x^4 + x^3 + x^2 + 1
-    vector<int> terms = {8, 4, 3, 2, 1};
+    vector<int> terms = {8, 4, 3, 2, 0};
     uint8_t mask = createPoly<uint8_t>(terms);
-    EXPECT_EQ(mask, 0b00011110);
+    EXPECT_EQ(mask, 0b00011101);
 } 
 
 TEST(Popcount, PopcountTest)
